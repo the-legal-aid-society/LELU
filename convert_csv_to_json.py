636749413b2152a7349af4d73feeaf011c702468
6W6ow9563Ppo8V484 <102563715+4V4ae23A25bL6k36Hw@users.noreply.github.com>
2023-06-09 04:11:11 -0400

    Update convert_csv_to_json.py

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -4,69 +4,63 @@ import csv
 import json
 import logging
 
-# Configure logger
-logger = logging.getLogger(__name__)  # create logger
-logger.setLevel(logging.INFO)  # set level of logger
+def setup_logger():
+    """Setup logger that outputs to console and a log file."""
 
-# Create file handler which logs even debug messages
-fh = logging.FileHandler("csv_to_json.log")
-fh.setLevel(logging.INFO)  # set level of file handler
+    logger = logging.getLogger(__name__)  # Create a logger with the module's name (__name__)
+    logger.setLevel(logging.INFO)  # Set the logger to capture all INFO and above level messages
 
-# Create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)  # set level of console handler
+    fh = logging.FileHandler('csv_to_json.log')  # Create a file handler to log messages to a file
+    fh.setLevel(logging.INFO)  # Set file handler to capture all INFO and above level messages
 
-# Create formatter and add it to the handlers
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+    ch = logging.StreamHandler()  # Create a console handler to log messages to the console
+    ch.setLevel(logging.INFO)  # Set console handler to capture all INFO and above level messages
 
-# Add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # Create a formatter that adds time, name, level and message to the logs
 
+    fh.setFormatter(formatter)  # Add the formatter to the file handler
+    ch.setFormatter(formatter)  # Add the formatter to the console handler
+
+    logger.addHandler(fh)  # Add the file handler to the logger
+    logger.addHandler(ch)  # Add the console handler to the logger
+
+    return logger  # Return the fully set up logger
+
+# Call setup_logger() to initialize the logger
+logger = setup_logger()
 
 def find_csv_files(directory):
-    # Using list comprehension, get list of csv files in directory and subdirectories
-    return [
-        os.path.join(root, file)
-        for root, dirs, files in os.walk(directory)
-        for file in files
-        if file.endswith(".csv")
-    ]
-
+    """Find all CSV files in a directory."""
+    # Use a list comprehension to find all CSV files in the specified directory and its subdirectories
+    return [os.path.join(root, file) for root, dirs, files in os.walk(directory) if file.endswith(".csv")]
 
 def convert_csv_to_json(csv_file_path, json_file_path):
+    """Convert a CSV file to JSON file."""
     try:
-        # Open the CSV file, read it and load it into a list of dictionaries
-        with open(csv_file_path, "r") as csv_file:
-            reader = csv.DictReader(csv_file)
-            csv_list = list(reader)
+        # Open the CSV file and read its contents into a list
+        with open(csv_file_path, 'r') as csv_file:
+            csv_list = list(csv.DictReader(csv_file))
 
-        # Open the JSON file (or create it) and write into it
-        with open(json_file_path, "w") as json_file:
+        # Open the JSON file (or create it) and write the contents of the list into it
+        with open(json_file_path, 'w') as json_file:
             json.dump(csv_list, json_file, indent=4)
 
+        # If conversion is successful, log an info message stating so
         logger.info(f"Converted {csv_file_path} to {json_file_path} successfully.")
-
     except Exception as e:
-        # If any error occurred while converting csv to json, log the error message and re-raise the exception
+        # If there's any error during the conversion process, log an error message stating so and re-raise the exception
         logger.error(f"Failed to convert {csv_file_path} to JSON: {e}")
         raise
 
-
 def main():
-    # Get list of csv files
-    csv_files = find_csv_files(".")
-
-    for csv_file in csv_files:
-        # Get the name of the file without the extension
-        json_file = os.path.splitext(csv_file)[0] + ".json"
-
-        # Convert each csv file to json
+    """Main function to find CSV files and convert them to JSON."""
+    # For each CSV file found in the current directory ('.')
+    for csv_file in find_csv_files('.'):
+        # Prepare the JSON file path by replacing the extension of the CSV file with '.json'
+        json_file = os.path.splitext(csv_file)[0] + '.json'
+        # Call the conversion function to convert the CSV file to a JSON file
         convert_csv_to_json(csv_file, json_file)
 
-
-# Python's way of running the main function
+# If this script is being run (as opposed to being imported), call the main() function
 if __name__ == "__main__":
     main()
